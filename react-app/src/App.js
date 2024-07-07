1f78a76af6d7fadac1fc0fa523cf1a2e56e24005
Amritaash Verma <amritaash.verma@gmail.com>
2024-07-07 16:11:50 +0530

    flowbite

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,36 @@
+// Use flowbite for pre made tailwind css components: https://flowbite.com/docs/getting-started/introduction/
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>A very simple react app</h1>
-      <h2 align="left">
+      <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">A very simple react app</h1>
+      <h3 align="left" class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         This button gives a counter of likes:
-        <p>
+        <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-white">
         <button>I am a button</button>
         </p>
-      </h2>
+      </h3>
+    
+
+<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="www.google.com">
+        <img class="rounded-t-lg" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thehansindia.com%2Ftechnology%2Ftech-news%2Ftoday-tech-news-updates-top-seven-things-to-know-on-31-july-2021-699091&psig=AOvVaw3Yy31--drKn2eyOvAUXid-&ust=1720435160591000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwiNlo_G3pSHAxWncWwGHXRVB8sQjRx6BAgAEBU" alt="" />
+    </a>
+    <div class="p-5">
+        <a href="www.google.com">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <a href="www.google.com" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>
+    </div>
+</div>
+
     </div>
   )
 }
---
+++ react-app/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/App.js',
+    './src/index.js',
+    'node_modules/flowbite-react/lib/esm/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
