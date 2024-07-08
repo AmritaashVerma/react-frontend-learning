fca57c8e53c0db5118a70fa1473d1283d412376a
Amritaash Verma <amritaash.verma@gmail.com>
2024-07-08 20:00:33 +0530

    ui improvisions

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,20 +5,22 @@ function App() {
   return (
     <div className="App">
       <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">A very simple react app</h1>
-      <h3 align="left" class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+      <h5 align="center" class="text-2xl text-gray-900 dark:text-white">
         This button gives a counter of likes:
         <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-white">
-        <button>I am a button</button>
+          <br></br>
+        <button class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">I am a button</button>
         </p>
-      </h3>
+    </h5>
     
 
+<center>
 <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="www.google.com">
-        <img class="rounded-t-lg" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thehansindia.com%2Ftechnology%2Ftech-news%2Ftoday-tech-news-updates-top-seven-things-to-know-on-31-july-2021-699091&psig=AOvVaw3Yy31--drKn2eyOvAUXid-&ust=1720435160591000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwiNlo_G3pSHAxWncWwGHXRVB8sQjRx6BAgAEBU" alt="" />
+        <img class="rounded-t-lg" src="https://free4kwallpapers.com/uploads/originals/2020/04/06/abstract-nature-art-wallpaper.jpg" alt="" />
     </a>
     <div class="p-5">
-        <a href="www.google.com">
+        <a href="https://www.google.com/">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
@@ -30,6 +32,7 @@ function App() {
         </a>
     </div>
 </div>
+</center>
 
     </div>
   )
